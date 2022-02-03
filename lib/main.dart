9ddcfa26a7c6c35f084248b8f4adb4172a941364
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-02-03 22:04:23 +0700

    feat(main): add provider for cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:counter_cubit/store/counter_cubit.dart';
 import 'package:counter_cubit/widgets/count.dart';
 import 'package:counter_cubit/widgets/floating_counter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    BlocProvider(
+      create: (context) => CounterCubit(),
+      child: const MyApp(),
+    )
+  );
 }
 
 class MyApp extends StatelessWidget {
