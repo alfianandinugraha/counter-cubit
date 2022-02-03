c56922a8a3035646397662ddd3ee6b123b3ef01c
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-02-03 20:36:08 +0700

    chore(app): init app

---
+++ android/app/src/main/kotlin/com/example/counter_cubit/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.counter_cubit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Counter Cubit',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Scaffold(
+        body: Text("Hello world !"),
+      ),
+    );
+  }
+}
