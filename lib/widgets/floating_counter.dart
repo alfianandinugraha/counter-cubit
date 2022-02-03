91e9eb62176f06b3528e49d6aad68145d013a776
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-02-03 22:30:17 +0700

    feat(floating_counter): disabled decrement if count <= 0

--- lib/widgets/floating_counter.dart
+++ lib/widgets/floating_counter.dart
@@ -2,6 +2,30 @@ import 'package:counter_cubit/store/counter_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+class DecrementCounter extends StatelessWidget {
+  const DecrementCounter({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocSelector<CounterCubit, CounterState, bool>(
+      selector: (state) {
+        return state.count <= 0;
+      },
+      builder: (context, isDisabled) {
+        return FloatingActionButton(
+          onPressed: () {
+            if (isDisabled) return;
+            BlocProvider.of<CounterCubit>(context).decrement();
+          },
+          child: const Icon(Icons.remove),
+          backgroundColor: isDisabled ? Colors.grey : Colors.blue,
+          splashColor: isDisabled ? Colors.grey : Theme.of(context).floatingActionButtonTheme.splashColor,
+        );
+      },
+    );
+  }
+}
+
 class FloatingCounter extends StatelessWidget {
   const FloatingCounter({Key? key}) : super(key: key);
 
@@ -17,12 +41,7 @@ class FloatingCounter extends StatelessWidget {
           child: const Icon(Icons.add),
         ),
         const SizedBox(height: 10),
-        FloatingActionButton(
-          onPressed: () {
-            BlocProvider.of<CounterCubit>(context).decrement();
-          },
-          child: const Icon(Icons.remove),
-        )
+        const DecrementCounter()
       ],
     );
   }
